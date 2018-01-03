401e5147f5280904b24b1f33002aa6e6e05bbff8
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2018-01-03 23:18:40 +0300

    bBRu9aCL: add pages (static: home, about, services, contacts)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def page_heading
+    params[:page].capitalize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Pages routes
+  root "pages#show", page: "home"
+  get "/*page" => "pages#show"
 end
