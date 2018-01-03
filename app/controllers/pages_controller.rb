8c09d3d568b26df4825135c9d66aa5cbfd11bd16
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2018-01-04 00:21:03 +0300

    bBRu9aCL: create Page model, dynamic home page content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
+    @page = Page.find_by_slug(params[:page])
     render template: "pages/#{params[:page]}"
   end
 end
---
+++ db/migrate/20180103205629_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.string :slug
+      t.string :desc
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
