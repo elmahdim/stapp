8ea63d2b517b09ac72a1f112a732972197fa6018
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2018-01-04 19:14:46 +0300

    bBRu9aCL: add generic/default page layout, to handle dynamic content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
+  rescue_from ActionView::MissingTemplate, with: :render_default_page
+
   def show
     @page = Page.find_by_slug(params[:page])
     render template: "pages/#{params[:page]}"
+  end
+
+  def render_default_page
+    render template: 'pages/page'
   end
 end
