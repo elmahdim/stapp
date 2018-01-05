84f355c46efbae5f0579bc587c8cf7edfcfd8c58
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2018-01-05 23:50:48 +0300

    GMpV1lop: add possibility to add new page from dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   rescue_from ActionView::MissingTemplate, with: :render_default_page
   before_action :current_page, only: [:edit, :update]
-  layout 'admin', only: [:edit, :update]
+  layout 'admin', only: [:edit, :update, :new]
 
   def show
     @page = Page.find_by!(slug: params[:page])
@@ -23,6 +23,21 @@ class PagesController < ApplicationController
           redirect_to edit_page_path(@current),
                       notice: ['Update Complete', 'Page was successfully updated.',
                                "View #{view_context.link_to(@current.title, root_path + @current.slug)}"]}
+      end
+    end
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    permitted_columns = params.require(:page).permit(:title, :desc, :content, :slug)
+    @page = Page.new(permitted_columns)
+
+    respond_to do |format|
+      if @page.save
+        format.html { redirect_to root_path + @page.slug, notice: 'Page was successfully created.' }
       end
     end
   end
